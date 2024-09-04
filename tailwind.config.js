6b6bf676abf9fbee2d42847c741be59e3a07db35
hungvannguyen <nguyenvanhung08012002@gmail.com>
2024-09-04 16:49:41 +0700

    update navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,15 @@ export default {
         'secondary':'#1F1F1F',
         'tertiary':'#111111',
         'quaternary':'#E6E6E6',
+        'quinary':'#FF9900',
+        'senary':'#7E7E7E',
+        'septenary':'#9B9B9B',
+        'octonary':'#181A20',
+        'nonary':'#EFEFEF',
       },
+      fontSize: {
+        xs: '0.625rem',
+      }
     },
   },
   plugins: [],
