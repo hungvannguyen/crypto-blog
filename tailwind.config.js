a7376c21cbac5b94258c0e788d49282da02e0815
hungvannguyen <nguyenvanhung08012002@gmail.com>
2024-09-09 10:12:06 +0700

    update home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,8 @@ export default {
         'septenary':'#9B9B9B',
         'octonary':'#181A20',
         'nonary':'#EFEFEF',
+        'denary':'#575757',
+        'eleven':'rgba(99, 99, 99, 0.1)',
       },
       fontSize: {
         xs: '0.625rem',
